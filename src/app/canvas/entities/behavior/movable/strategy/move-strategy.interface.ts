ab0f22efa37caec4788f202b21f77c68c3d9b0d0
nielsdelestinne <nielsdelestinne@gmail.com>
2017-12-27 16:05:50 +0100

    Remove broken import

--- src/app/canvas/entities/behavior/movable/strategy/move-strategy.interface.ts
+++ src/app/canvas/entities/behavior/movable/strategy/move-strategy.interface.ts
@@ -1,6 +1,4 @@
 import {Point} from "../../../point.class";
-import {Entity} from "../../../entity.interface";
-import {BaseShape} from "../../../shapes/base/shape.abstract";
 import {MovableShape} from "../../../shapes/movable/movable.shape.class";
 
 export interface MoveStrategy {
